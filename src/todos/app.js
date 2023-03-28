2dd70b823448dc640ea18ee796cbb46baaadd2ed
Mauricio Suarez <mauriciosuarezcalderon@gmail.com>
2023-03-28 15:02:02 -0500

    clase 108

---
+++ src/todos/app.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {String} elementId 
+ */
+
+import html from './app.html?raw'
+
+export const App = ( elementId ) => {
+
+
+    //Cuando la funcion app se llama
+    (()=> {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append( app );
+    })()
+}
